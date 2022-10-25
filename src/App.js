37d02d955c5770ef6f343b97fdfd99d0b13d822c
Aditya <adityasinghboss1234@gmail.com>
2022-10-25 20:21:16 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import "./App.css";
 
 function App(){
     return (
-      <div></div>
+      <div>
+        <nav>
+        <ul className="container">
+          <li>Home</li>  
+          <li>Profile</li> 
+          <li>Logout</li>
+        </ul>
+        </nav>
+      </div>
     );
 };
 export default App;
